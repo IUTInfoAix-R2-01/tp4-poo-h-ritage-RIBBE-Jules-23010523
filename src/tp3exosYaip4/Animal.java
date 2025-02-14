3cb13c8021eb2b4d8c145e2fe98b3857f6c3cdb8
RIBBE-Jules-23010523 <jules.ribbe@etu.univ-amu.fr>
2025-02-14 17:33:32 +0100

    Update Animal.java

--- src/tp3exosYaip4/Animal.java
+++ src/tp3exosYaip4/Animal.java
@@ -1,14 +1,19 @@
 package tp3exosYaip4;
 
 public class Animal {
-	private String name;
-	
-	public Animal(String name) {
-		this.name = name;
-	}
+    private String name;
 
-	@Override
-	public String toString() {
-		return "Animal [name=" + name + "]";
-	}
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal[name=" + name + "]";
+    }
+
+    public static void main(String[] args) {
+        Animal a = new Animal("Buddy");
+        System.out.println(a);
+    }
 }
